b27fb33abc64930b7967286acf8d1501807a49ba
JackInABot <bartlettjack97@gmail.com>
2020-02-09 14:21:20 +0000

    quick listed different sdk instcnaces

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import anki_vector
 #get utility paths
 from Utilities import RainbowEyes as rbe
-from Utilities import Megaphone as megPhone
+from Utilities import Megaphone as mph
 
 def main():
     # d^-^b vars for the test d^-^b #
@@ -13,15 +13,18 @@ def main():
     with anki_vector.Robot(args.serial) as robot:
         # d^-^b Any initial setup before test d^-^b #
         robotEyes = rbe.RainbowEyes(robot)
-        robotVoice = megPhone.Megaphone(robot)
+        robotVoice = mph.Megaphone(robot)
 
         # end init #
         print("Beginning Test...")
         while True:
             # d^-^b Test Contents d^-^b #
-            #robotEyes.MakeEyesRainbow(0.1, 10)
-            robotVoice.Say()
-            #robotVoice.PlayAudioList(False,80)
+            with anki_vector.Robot() as robot:
+                print("List all animation trigger names:")
+                anim_trigger_names = robot.anim.anim_trigger_list
+                for anim_trigger_name in anim_trigger_names:
+                    print(anim_trigger_name)
+
             # end test # 
             if(forever == False):
                 break
@@ -30,7 +33,29 @@ def main():
         print("Test Complete. Returning Vector to his common state... \n\n\n\n\n")
         time.sleep(3)
 
+def setupType1(funcRef):
+    args = anki_vector.util.parse_command_args()
+    with anki_vector.Robot(args.serial) as robot:
+        funcRef()
+
 
 if __name__ == '__main__':
     main()
 
+
+
+
+
+"""
+Setup type one (with args):
+---------------------------------------------------------------------------
+    args = anki_vector.util.parse_command_args()
+    with anki_vector.Robot(args.serial) as robot:
+
+Setup type two (behaviour None)
+Works only with - SetGlobalVolume()
+---------------------------------------------------------------------------
+    with anki_vector.Robot(behavior_control_level=None) as robot:
+
+
+"""
